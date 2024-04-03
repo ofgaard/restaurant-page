b24ea79f6ac02e4bbc181960a7f8395ee70ae378
Oliver <oliverfruergaard@gmail.com>
2024-04-03 12:09:55 +0200

    setup index.html in src, add header, background image, navbar

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [new HtmlWebpackPlugin(
-    {template: './src/index.html',
-})],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   module: {
     rules: [
       {
